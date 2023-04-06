730fafebf03edc5636830bbda7417891ff4cf1ee
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-04-06 18:47:22 +0200

    Create logic for removeFromArray function
    Added code to the removeFromArray function to remove the values of
    additional arguments to the function from an array that is also
    passed through arguments.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayItems = [1,2,3,4]) {
+
+	if (arguments.length > 1) {
+		// get the value of the extra arguments
+		// for loop to remove a value from the array for each additional argument
+		for (let i = 1; i < arguments.length; i++) {
+			// Checks if the argument value exists in the given array, if not, this iteration of the loop
+			// will be skipped over
+			if (!arrayItems.includes(arguments[i])) {
+				continue;
+			} else {
+			// Variable to get the index of additional argument
+			let index = arrayItems.indexOf(arguments[i]);
+			arrayItems.splice(index, 1);
+			}
+		}
+	}
+
+	return arrayItems;
 
 };
 
