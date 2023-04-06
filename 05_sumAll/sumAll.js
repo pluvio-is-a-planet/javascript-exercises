38fb7881212e74294c003bb92b433581c7c45841
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-04-06 19:02:22 +0200

    Create logic for sumAll function
    Created function to accept 2 parameters, and calculate the total sum of
    all integer numbers between (and including) them. Works with the larger
    number as the first or second argument.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+
+    // Initialize variable to store the total
+  let totalSum = 0;
+
+    // Check if the startNum variable is larger than the endNum variable, if so, swap the variables
+  if (startNum > endNum) {
+        
+    let swapNum = startNum;
+    startNum = endNum;
+    endNum = swapNum;
+
+  } else if (startNum < 0 || endNum < 0) {
+    return 'ERROR';
+  } else if (typeof(startNum) != 'number' || typeof(endNum) != 'number') {
+		return 'ERROR';
+	}
+	
+  // Loop that iterates from the starting number to the ending number (inclusive of)
+  for (let i = startNum; i <= endNum; i++) {
+  // For each iteration, add the value of the iterator to a variable to store the total
+  totalSum += i;
+  }
+  // Return the total
+  return totalSum;
 
 };
 
