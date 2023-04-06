addccafb53850b3055bafb76f63016432ebd9d07
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-04-06 16:40:16 +0200

    Create function according to test file

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+	stringReversed = '';
+// Loop to go through each character of given string, from the end to the start of the string
+for (let i = string.length -1; i >= 0; i--) {
+// Code to extract a character from each iteration of the loop,
+// and append it to a string that will be returned
+	stringReversed += string[i];
+
+}
+
+	return stringReversed;
 
 };
 
