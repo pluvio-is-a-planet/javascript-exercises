5d9dfa2633934e8c760b996e589be8834fbbeacf
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-04-06 19:34:03 +0200

    Create logic for leapYears function
    Created logic to determine if the year (given as an argument) is a
    leap year, according to the logic that a year divisible by 4, and 400,
    but not by 100 and every other number, is a leap year.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+	// If year is divisble by 4, or 400 it is a leap year, else if it is divisible by 100 it is not
+	if (year % 100 === 0) {
+		if (year % 400 === 0) {
+			return true;
+		} else {
+			return false;
+		}
+	} else if (year % 4 === 0) {
+		return true;
+	} else {
+		return false;
+	}
 
 };
 
